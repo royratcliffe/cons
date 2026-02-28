7d93fe7b4deabc334ffe4cd37b9caf14413499fa
Roy Ratcliffe <roy@ratcliffe.me>
2026-02-28 12:26:22 +0000

    Add nested cons test cases and update CMake configuration
    Introduce a new test file for nested cons operations, demonstrating
    the use of nested cons to build lists in various scenarios. Update
    the CMake configuration to include the new test in the test runner.

--- test/cons.c
+++ test/cons.c
@@ -45,20 +45,5 @@ int test_cons(int argc, char *argv[]) {
     assert(CONS_NIL_P(cons_cdr(&cell3)));
   }
 
-  {
-    struct cons cell1 = CONS((void *)1, CONS_NIL);
-    struct cons cell2 = CONS((void *)2, CONS_NIL);
-    struct cons cell3 = CONS((void *)3, CONS_NIL);
-    struct cons *list = CONS_NIL;
-    cons(cons(cons(&list, &cell1), &cell2), &cell3);
-    for (struct cons *cell = list; CONS_NOT_NIL_P(cell); cell = cons_cdr(cell)) {
-      assert((uintptr_t)cons_car(cell) == 1 || (uintptr_t)cons_car(cell) == 2 || (uintptr_t)cons_car(cell) == 3);
-    }
-    assert(list == &cell1);
-    assert(cons_cdr(&cell1) == &cell2);
-    assert(cons_cdr(&cell2) == &cell3);
-    assert(CONS_NIL_P(cons_cdr(&cell3)));
-  }
-
   return EXIT_SUCCESS;
 }
