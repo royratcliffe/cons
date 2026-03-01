f0147d71cbb70a3b3f2d6e56c63f314b9ff233da
Roy Ratcliffe <roy@ratcliffe.me>
2026-03-01 05:37:18 +0000

    Refactor delete tests to use 'head' instead of 'list'
    Updated variable names in test_delete and test_nested_cons
    functions for consistency and clarity. Changed 'list' to 'head'
    to better reflect the purpose of the variable.

--- test/delete.c
+++ test/delete.c
@@ -9,18 +9,18 @@ int test_delete(int argc, char *argv[]) {
   (void)argv;
 
   {
-    struct cons *list = CONS_NIL;
-    assert(CONS_NIL_P(cons_delete(&list, NULL)));
+    struct cons *head = CONS_NIL;
+    assert(CONS_NIL_P(cons_delete(&head, NULL)));
   }
 
   {
     struct cons cell1 = CONS((void *)1, CONS_NIL);
     struct cons cell2 = CONS((void *)2, CONS_NIL);
     struct cons cell3 = CONS((void *)3, CONS_NIL);
-    struct cons *list = CONS_NIL;
-    cons(cons(cons(&list, &cell1), &cell2), &cell3);
-    assert(cons_delete(&list, (void *)1) == &cell1);
-    assert(list == &cell2);
+    struct cons *head = CONS_NIL;
+    cons(cons(cons(&head, &cell1), &cell2), &cell3);
+    assert(cons_delete(&head, (void *)1) == &cell1);
+    assert(head == &cell2);
     assert(cons_cdr(&cell2) == &cell3);
     assert(CONS_NIL_P(cons_cdr(&cell3)));
   }
@@ -29,12 +29,12 @@ int test_delete(int argc, char *argv[]) {
     struct cons cell1 = CONS((void *)1, CONS_NIL);
     struct cons cell2 = CONS((void *)2, CONS_NIL);
     struct cons cell3 = CONS((void *)3, CONS_NIL);
-    struct cons *list = CONS_NIL;
-    cons(&list, &cell1);
-    cons(&list, &cell2);
-    cons(&list, &cell3);
-    assert(cons_delete(&list, (void *)2) == &cell2);
-    assert(list == &cell3);
+    struct cons *head = CONS_NIL;
+    cons(&head, &cell1);
+    cons(&head, &cell2);
+    cons(&head, &cell3);
+    assert(cons_delete(&head, (void *)2) == &cell2);
+    assert(head == &cell3);
     assert(cons_cdr(&cell3) == &cell1);
     assert(CONS_NIL_P(cons_cdr(&cell1)));
   }
--- test/nested_cons.c
+++ test/nested_cons.c
@@ -37,11 +37,11 @@ int test_nested_cons(int argc, char *argv[]) {
     struct cons cell1 = CONS((void *)1, CONS_NIL);
     struct cons cell2 = CONS((void *)2, CONS_NIL);
     struct cons cell3 = CONS((void *)3, CONS_NIL);
-    struct cons *list = CONS_NIL;
-    cons(cons(&list, &cell1), &cell2);
-    cons(&list, &cell3);
+    struct cons *head = CONS_NIL;
+    cons(cons(&head, &cell1), &cell2);
+    cons(&head, &cell3);
 
-    assert(list == &cell3);
+    assert(head == &cell3);
     assert(cons_cdr(&cell3) == &cell1);
     assert(cons_cdr(&cell1) == &cell2);
     assert(CONS_NIL_P(cons_cdr(&cell2)));
